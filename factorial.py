938c9628f11e985b71fee613303c95acfbf4bed1
Parash khadka <parashkhadka020@gmail.com>
2025-06-29 22:59:31 +0545

    Assignment

---
+++ factorial.py
@@ -0,0 +1,6 @@
+#question no 8
+def factoriall(n):
+    result=1
+    for i in range(1,n+1):
+        result*=i
+    return result  
---
+++ main.py
@@ -0,0 +1,4 @@
+#question no 8
+import factorial
+number=int(input("enter the number you want ot find factorial of :"))
+print(f"the factorial of the {number} is {factorial.factoriall(number)}")
